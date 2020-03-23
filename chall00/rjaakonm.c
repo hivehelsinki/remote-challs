cd197d6494916cd2d626e3e952a948dbf1135d63
rasmus <danzel89@gmail.com>
2020-03-23 15:22:33 +0200

    chall00 solved with testing all possible rot1

---
+++ chall00/rjaakonm.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char	*rotone(char *s1)
+{
+	int len;
+	int i;
+	char x;
+	char *str;
+  
+	len = strlen(s1);
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	str[len] = 0;
+	x = s1[len - 1];
+	i = len - 1;
+	while(i > 0)
+	{
+		str[i] = s1[i - 1];
+		i--;
+	}
+	str[0] = x;
+	return (str);
+}
+
+int hv_necklace(char *s1, char *s2)
+{
+	int i;
+	int len;
+	char *tmp;
+	char *tmp2;
+
+	len = strlen(s1);
+	i = 0;
+	tmp = rotone(s1);
+	while(i < len)
+	{
+		if (strcmp(tmp, s2) == 0)
+		{
+			free(tmp);
+			return (0);
+		}
+		tmp2 = tmp;
+		tmp = rotone(tmp);
+		free(tmp2);
+		i++;
+	}
+	return (1);
+}

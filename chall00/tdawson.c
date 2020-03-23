12ddcdaf34bd9e036edef972e523e976e25f5db5
tdawson-42 <tdawson@student.hive.fi>
2020-03-23 15:15:16 +0200

    submit solution

---
+++ chall00/tdawson.c
@@ -0,0 +1,17 @@
+#include <string.h>
+#include <stdlib.h>
+
+int hv_necklace(char *s1, char *s2)
+{
+	char *s1s1;
+
+	if (!s1 || !s2)
+		return (0);
+	if (!(s1s1 = malloc(strlen(s1) * 2 + 1)))
+		return (0);
+	strcpy(s1s1, s1);
+	strcat(s1s1, s1);
+	int ret = (strlen(s1) == strlen(s2)) && strstr(s1s1, s2);
+	free(s1s1);
+	return ret;
+}

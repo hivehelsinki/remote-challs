a5a22ae71f5deee7151f6a56b60edf80e397aa15
Jochum Wilen <jochumwilen@MacBook-Pro-som-tillhor-Jochum.local>
2020-03-24 10:06:50 +0200

    error fixed thanks to Jiri

--- chall00/jwilen.c
+++ chall00/jwilen.c
@@ -6,12 +6,9 @@
 /*   By: jochumwilen <jochumwilen@student.le-101.fr>+#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/23 21:59:45 by jochumwilen       #+#    #+#             */
-/*   Updated: 2020/03/23 21:59:45 by jochumwilen      ###   ########lyon.fr   */
+/*   Updated: 2020/03/24 10:05:56 by jochumwilen      ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <stdlib.h>
-#include <unistd.h>
 
 int     ft_strlen(char *str)
 {
@@ -29,34 +26,30 @@ int     hv_necklace(char *s1, char *s2)
     int     len_s2;
     int     j;
     int     k;
+    int		s;
 
     j = 0;
-    k = 0;
+    s = 0;
     len_s1 = ft_strlen(s1);
     len_s2 = ft_strlen(s2);
     if (len_s1 != len_s2)
         return (0);
     if (s1 == s2)
         return (1);
-    while (s1[j])
-    {
-        while (s2[k])
-        {
-            while (s1[j] == s2[k])
-            {
-                j++;
-                k++;
-                if (s2[k] == '\0')
-                {
-                    k = 0;
-                }
-                if (s1[j] == '\0')
-                    return (1);
-            }
-            k++;
-            j = (s1[j-1] == s2[k-1]) ? 0 : j;
-        }
-        j++;
-    }
+    while (s2[s])
+	{
+    	k = s;
+    	while (s1[j] == s2[k])
+    	{
+    		j++;
+    		k++;
+    		if (s2[k] == '\0')
+    			k = 0;
+    		if (s1[j] == '\0')
+    			return (1);
+    	}
+    	s++;
+    	j = 0;
+	}
     return(0);
 }

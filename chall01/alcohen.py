40b95ba19fe86fc9640197c62f3dfb6d6119cac6
hiiri <aleksicohen@gmail.com>
2020-03-26 13:33:03 +0200

    Fix error in input handling when reading empty strings

--- chall01/alcohen.py
+++ chall01/alcohen.py
@@ -28,14 +28,22 @@ c = """.-
 -..- 
 -.-- 
 --.."""
-string_alpha_or_spaces = all(x.isalpha() or x.isspace() for x in sys.argv[1])
-if (len(sys.argv) != 2 or not string_alpha_or_spaces or not sys.argv[1]):
+
+print(sys.argv[1])
+def error():
     print("usage: ./alcohen.py <a-zA-Z string>")
     exit()
+
+if (len(sys.argv) != 2):
+    error()
+
+string_alpha_or_spaces = all(x.isalpha() or x.isspace() for x in sys.argv[1])
+if (not string_alpha_or_spaces or not sys.argv[1]):
+    error()
+
 codes = c.split()
 alph = list(string.ascii_lowercase)
 d = dict(zip(alph, codes))
-
 
 for x in str(sys.argv[1]).lower():
     if (x == " "):

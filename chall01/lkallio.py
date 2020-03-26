a15e51b61d9d94cc6af72ad8491cbfcedde0e2a2
Lalli Kallio <lallikallioo@gmail.com>
2020-03-26 23:09:18 +0200

    Now takes spaces into account -.-

--- chall01/lkallio.py
+++ chall01/lkallio.py
@@ -21,15 +21,21 @@ def		search_morse_tree(inp, i, j):
 	if (i > 28):
 		return (0)
 	elif (i and (alpha_tree[i] == inp[j]
-	or alpha_tree[i].upper() == inp[j] * 2)):
+	or alpha_tree[i].upper() == inp[j]
+	or inp[j] == ' ')):
 		if (j < len(inp) - 1):
 			if (not search_morse_tree(inp, 0, j + 1)):
+				print_usage()
+	elif (i and (inp[j] == ' ')):
+		if (not search_morse_tree(inp, 0, j + 1)):
 				print_usage()
 	elif (not search_morse_tree(inp, i * 2 + 1, j)
 	and not search_morse_tree(inp, i * 2 + 2, j)):
 		return (0)
-	if (i):
+	if (i and inp[j] != ' '):
 		output.append('.' if i % 2 else '-')
+	elif (i):
+		output.append(' ')
 	return (1)
 
 if (len(sys.argv) != 2 or not len(sys.argv[1])

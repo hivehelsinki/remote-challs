7df89be4c056194d751ac6ac5c7579307cd18056
vgrankul <vgrankul@debian.com>
2020-03-27 01:12:55 +0200

    added vgrankul.py

---
+++ chall01/vgrankul.py
@@ -0,0 +1,50 @@
+#!/bin/python3
+import sys
+
+sos = [
+".-",
+"-...", 
+"-.-.", 
+"-..", 
+".", 
+"..-.", 
+"--.", 
+"....", 
+"..", 
+".---", 
+"-.-", 
+".-..", 
+"--", 
+"-.", 
+"---", 
+".--.", 
+"--.-", 
+".-.", 
+"...", 
+"-", 
+"..-", 
+"...-", 
+".--", 
+"-..-", 
+"-.--", 
+"--.."]
+
+i = 0
+letter = ['a', 'b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+Bletter = ['A', 'B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+sos_string = ""
+
+if len(sys.argv) != 2:
+    print("usage: ./vgrankul.py <a-zA-Z string>")
+else:
+    string = sys.argv[1]
+    if string.replace(' ', '').isalpha() == False:
+        print("usage: ./vgrankul.py <a-zA-Z string>")
+        exit()
+    for let in string:
+            i = 0
+            while i < 26 and i < 26:
+                if letter[i] == let or Bletter[i] == let:
+                    sos_string += sos[i]
+                i += 1
+    print(sos_string)

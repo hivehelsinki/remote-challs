4cb7e11e0eb66f2219bbc366acb9e8c84c17e46b
rasmusjaa <rasmus@lense.fi>
2020-03-26 22:07:48 +0200

    working string to morse

---
+++ chall01/rjaakonm.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+import sys
+
+morse = [".-",
+	"-...",
+	"-.-.",
+	"-..",
+	".",
+	"..-.",
+	"--.",
+	"....",
+	"..",
+	".---",
+	"-.-",
+	".-..",
+	"--",
+	"-.",
+	"---",
+	".--.",
+	"--.-",
+	".-.",
+	"...",
+	"-",
+	"..-",
+	"...-",
+	".--",
+	"-..-",
+	"-.--",
+	"--.."]
+
+def main():
+	str = ""
+	if (len(sys.argv) != 2) or (len(sys.argv[1]) == 0):
+		print("usage: ./rjaakonm.py <a-zA-Z string>")
+		return
+	sys.argv[1] = sys.argv[1].lower()
+	for char in sys.argv[1]:
+		if char.isalpha():
+			str += morse[ord(char) - 97]
+		elif char == " ":
+			str += " "
+		else:
+			print("usage: ./rjaakonm.py <a-zA-Z string>")
+			return
+	print(str)
+
+if __name__ == "__main__":
+	main() 

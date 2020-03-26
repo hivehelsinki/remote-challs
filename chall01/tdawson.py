f10f050ecd134384afd290f8de1c8167ac0519a5
tdawson-42 <tdawson@student.hive.fi>
2020-03-26 20:03:37 +0200

    a little more refactoring

--- chall01/tdawson.py
+++ chall01/tdawson.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 import sys
 
-def error():
-    print(f'usage: {sys.argv[0]} <a-zA-Z string>')
-    sys.exit()
-
 if len(sys.argv) != 2 or not isinstance(sys.argv[1], str) or len(sys.argv[1]) == 0:
-    error()
+    sys.exit(f'usage: {sys.argv[0]} <a-zA-Z string>')
 
 morse_alphabet = {'a': '.-', 'b': '-...', 'c': '-.-.', 'd': '-..', 'e': '.',
                   'f': '..-.', 'g': '--.', 'h': '....', 'i': '..', 'j': '.---',
@@ -17,9 +13,8 @@ morse_alphabet = {'a': '.-', 'b': '-...', 'c': '-.-.', 'd': '-..', 'e': '.',
 string = sys.argv[1].lower()
 
 if not all(c in morse_alphabet for c in string):
-    error()
+    sys.exit(f'usage: {sys.argv[0]} <a-zA-Z string>')
 
 for c in string:
     print(morse_alphabet[c], end='')
-else:
-    print('')
+print('')

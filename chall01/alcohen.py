0d25251521462a52b578d9aff727ed3d92594cfb
alcohen@student.hive.fi <alcohen@student.hive.fi>
2020-03-26 18:33:51 +0200

    Clear code up by adding space to alphabet list

--- chall01/alcohen.py
+++ chall01/alcohen.py
@@ -12,12 +12,13 @@ morse_codes = ['.-', '-...', '-.-.', '-..', '.', '..-.', '--.', '....', '..', '.
 if (len(sys.argv) != 2): # Only accept one argument
     error()
 
-alphabet = list(string.ascii_lowercase)
+# alphabet contains letters [a-z] and a space
+alphabet = list(string.ascii_lowercase + ' ')
 string_to_convert = sys.argv[1].lower()
 
-# Check that string to convert isn't empty and only contains letters [a-zA-Z] and spaces
-string_a_to_z_or_spaces = all(x.lower() in alphabet or x.isspace() for x in string_to_convert) 
-if (not string_a_to_z_or_spaces or not string_to_convert):
+# Check that string to convert isn't empty and only contains characters that are in alphabet.
+string_is_a_to_z_or_spaces = all(letter in alphabet for letter in string_to_convert) 
+if (not string_is_a_to_z_or_spaces or not string_to_convert):
     error()
 
 # Combine letters and morse codes to a dictionary. A letter can be looked up to receive the corresponding morse code.

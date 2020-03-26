ba408a89ca7a8bf773d00c4fafeb3c434d491851
tdawson-42 <tdawson@student.hive.fi>
2020-03-26 16:44:59 +0200

    refactoring and changed code formatting

--- chall01/tdawson.py
+++ chall01/tdawson.py
@@ -1,47 +1,25 @@
 #!/usr/bin/python3
 import sys
 
-if len(sys.argv) != 2 or not isinstance(sys.argv[1], str) or len(sys.argv[1]) == 0:
-	print("usage: ./xlogin.py <a-zA-Z string>")
-	sys.exit()
+def error():
+    print('usage: ./xlogin.py <a-zA-Z string>')
+    sys.exit()
 
-morse_alphabet = {
-	"a" : ".-",
-	"b" : "-...",
-	"c" : "-.-.",
-	"d" : "-..",
-	"e" : ".",
-	"f" : "..-.",
-	"g" : "--.",
-	"h" : "....",
-	"i" : "..",
-	"j" : ".---",
-	"k" : "-.-",
-	"l" : ".-..",
-	"m" : "--",
-	"n" : "-.",
-	"o" : "---",
-	"p" : ".--.",
-	"q" : "--.-",
-	"r" : ".-.",
-	"s" : "...",
-	"t" : "-",
-	"u" : "..-",
-	"v" : "...-",
-	"w" : ".--",
-	"x" : "-..-",
-	"y" : "-.--",
-	"z" : "--..",
-	" " : " "
-}
+if len(sys.argv) != 2 or not isinstance(sys.argv[1], str) or len(sys.argv[1]) == 0:
+    error()
+
+morse_alphabet = {'a': '.-', 'b': '-...', 'c': '-.-.', 'd': '-..', 'e': '.',
+                  'f': '..-.', 'g': '--.', 'h': '....', 'i': '..', 'j': '.---',
+                  'k': '-.-', 'l': '.-..', 'm': '--', 'n': '-.', 'o': '---', 'p': '.--.',
+                  'q': '--.-', 'r': '.-.', 's': '...', 't': '-', 'u': '..-', 'v': '...-',
+                  'w': '.--', 'x': '-..-', 'y': '-.--', 'z': '--..', ' ': ' '}
 
 string = sys.argv[1].lower()
 
 if not all(c in morse_alphabet for c in string):
-	print("usage: ./xlogin.py <a-zA-Z string>")
-	sys.exit()
+    error()
 
 for c in string:
-	print(morse_alphabet[c], end="")
+    print(morse_alphabet[c], end='')
 else:
-	print("")
+    print('')

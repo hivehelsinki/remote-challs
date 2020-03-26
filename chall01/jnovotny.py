979b720fa895ddf46650c0e867dcce09ed2faf43
Jiri Novotny <jnovotny@student.hive.fi>
2020-03-26 15:14:36 +0200

    upload solution

---
+++ chall01/jnovotny.py
@@ -0,0 +1,47 @@
+import sys
+
+def morse(string):
+	morse_dict = {
+		"a":".-",
+		"b":"-...",
+		"c":"-.-.", 
+		"d":"-..",
+		"e":".",
+		"f":"..-.", 
+		"g":"--.",
+		"h":"....",
+		"i":"..",
+		"j":".---",
+		"k":"-.-",
+		"l":".-..",
+		"m":"--",
+		"n":"-.",
+		"o":"---",
+		"p":".--.",
+		"q":"--.-",
+		"r":".-.",
+		"s":"...",
+		"t":"-",
+		"u":"..-",
+		"v":"...-",
+		"w":".--",
+		"x":"-..-",
+		"y":"-.--",
+		"z":"--.."}
+	str_out = ""
+	for letter in string:
+		if letter == " ":
+			str_out += " "
+		else:
+			str_out += f"{morse_dict[letter]}"
+	return str_out
+
+if __name__ == "__main__":
+	if len(sys.argv) == 2:
+		for word in  sys.argv[1].split():
+			if not word.isalpha():
+				print("usage: ./jnovotny.py <a-zA-Z string>")
+				exit()
+		print(morse(sys.argv[1].lower()))
+	else:
+		print("usage: ./jnovotny.py <a-zA-Z string>")

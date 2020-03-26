96734176cc26604dbc08e4ba8a72de83c58914a5
Lalli Kallio <lallikallioo@gmail.com>
2020-03-26 21:34:01 +0200

    ?

---
+++ chall01/lkallio.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+import sys
+
+output = []
+alpha_tree = "/etianmsurwdkgohvfulapjbxcyzq"
+
+def		print_usage():
+	print 'usage: ./lkallio.py <a-zA-Z string>'
+	exit(0)
+
+# We search the string "alpha_tree" such that every recursive step
+# "left" (i is uneven) signifies a dot, whereas a step "right"
+# signifies a line. When we hit the character, we either step to the next 
+# character or we return 1 and begin appending dots or lines every step of the way
+# back from the recursion, creating a reversed string of morse code.
+# The source for this idea is this image:
+# https://qph.fs.quoracdn.net/main-qimg-fd6688d09555dd7cf9221e7396377c5b
+
+def		search_morse_tree(inp, i, j):
+	if (i > 28):
+		return (0)
+	elif (i and (alpha_tree[i] == inp[j]
+	or alpha_tree[i].upper() == inp[j] * 2)):
+		if (j < len(inp) - 1):
+			if (not search_morse_tree(inp, 0, j + 1)):
+				print_usage()
+	elif (not search_morse_tree(inp, i * 2 + 1, j)
+	and not search_morse_tree(inp, i * 2 + 2, j)):
+		return (0)
+	if (i):
+		output.append('.' if i % 2 else '-')
+	return (1)
+
+if (len(sys.argv) != 2 or not len(sys.argv[1])
+or not search_morse_tree(sys.argv[1], 0, 0)):
+	print_usage()
+
+print ''.join(output[::-1])

e1445d8b69f42857bbd9c8ff0439b9632ad73967
Juha Hakala <57968342+Cheatteri@users.noreply.github.com>
2020-03-26 15:36:43 +0200

    Create jhakala.py

---
+++ chall01/jhakala.py
@@ -0,0 +1,33 @@
+MORSE = { 'A':'.-', 'B':'-...', 'C':'-.-.', 'D':'-..', 'E':'.', 
+        'F':'..-.', 'G':'--.', 'H':'....', 'I':'..', 'J':'.---',
+        'K':'-.-', 'L':'.-..', 'M':'--', 'N':'-.', 'O':'---',
+        'P':'.--.', 'Q':'--.-', 'R':'.-.', 'S':'...', 'T':'-', 
+        'U':'..-', 'V':'...-', 'W':'.--', 'X':'-..-', 'Y':'-.--',
+        'Z':'--..', 'a':'.-', 'b':'-...', 'c':'-.-.', 'd':'-..',
+        'e':'.', 'f':'..-.', 'g':'--.', 'h':'....', 'i':'..',
+        'j':'.---', 'k':'-.-', 'l':'.-..', 'm':'--', 'n':'-.',
+        'o':'---', 'p':'.--.', 'q':'--.-', 'r':'.-.', 's':'...',
+        't':'-', 'u':'..-', 'v':'...-', 'w':'.--', 'x':'-..-',
+        'y':'-.--', 'z':'--..'}
+
+def tomorse(message): 
+    output = "" 
+    for letter in message: 
+        if letter == ' ':
+            output += " "
+        elif letter.isalpha():
+            output += MORSE[letter]
+        else:
+            print("usage: ./jhakala.py <a-zA-Z string>")
+            return
+    print(output)
+    return
+
+def main():
+    if len(sys.argv) != 2 or len(sys.arg[1]) == 0:
+        print("usage: ./jhakala.py <a-zA-Z string>")
+        return
+    tomorse(argv[1])
+
+if __name__ == '__main__': 
+    main() 

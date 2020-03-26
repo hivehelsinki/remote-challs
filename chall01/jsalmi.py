a0e526a6c67542ab3a14ea26ecb2941e5aa994ed
Jony Salmi <jony.salmi97@gmail.com>
2020-03-26 19:32:30 +0200

    i lied, this is final

--- chall01/jsalmi.py
+++ chall01/jsalmi.py
@@ -29,7 +29,7 @@ alpha = {
         'y' : "-.--",
         'z' : "--..",
         ' ' : " "}
-if len(sys.argv) == 2 and len(sys.argv[1]) > 0 and all(i.isalpha() or i.isspace() for i in sys.argv[1]):
+if len(sys.argv) == 2 and len(sys.argv[1]) > 0 and all(i in alpha for i in sys.argv[1]):
     for letter in sys.argv[1]:
         print(alpha[letter.lower()], end = '')
     print('')

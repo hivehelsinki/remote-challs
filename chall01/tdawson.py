d101567b3c00c169e499528aafc064a42f2cf341
tdawson-42 <tdawson@student.hive.fi>
2020-03-26 16:06:08 +0200

    fixed errors with special characters such as öäå

--- chall01/tdawson.py
+++ chall01/tdawson.py
@@ -1,17 +1,9 @@
 #!/usr/bin/python3
 import sys
 
-if len(sys.argv) != 2:
+if len(sys.argv) != 2 or not isinstance(sys.argv[1], str) or len(sys.argv[1]) == 0:
 	print("usage: ./xlogin.py <a-zA-Z string>")
 	sys.exit()
-
-string = sys.argv[1]
-
-if not all(c.isalpha() or c.isspace() for c in string) or len(string) == 0:
-	print("usage: ./xlogin.py <a-zA-Z string>")
-	sys.exit()
-
-string = string.lower()
 
 morse_alphabet = {
 	"a" : ".-",
@@ -42,6 +34,12 @@ morse_alphabet = {
 	"z" : "--..",
 	" " : " "
 }
+
+string = sys.argv[1].lower()
+
+if not all(c in morse_alphabet for c in string):
+	print("usage: ./xlogin.py <a-zA-Z string>")
+	sys.exit()
 
 for c in string:
 	print(morse_alphabet[c], end="")

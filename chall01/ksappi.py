36da08d50986cf3a5c57bda0bd506b991d92fa4e
kimsappi <sappikim@gmail.com>
2020-03-26 05:57:13 -0700

    Fixed special cases

--- chall01/ksappi.py
+++ chall01/ksappi.py
@@ -7,13 +7,17 @@ charset = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..",
 
 def main():
 	output = ""
-	if len(sys.argv) < 2:
+	if len(sys.argv) != 2 or len(sys.argv[1]) == 0:
+		print("usage: ./ksappi.py <a-zA-Z string>")
 		return
 	for char in sys.argv[1]:
 		if char.isalpha():
 			output += charset[ord(char.lower()) - ord('a')]
 		elif char == " ":
 			output += " "
+		else:
+			print("usage: ./ksappi.py <a-zA-Z string>")
+			return
 	print(output)
 
 if __name__ == "__main__":

603d958fde6ad2779040c45b1ebc0ad5af207ac9
Aleksi Cohen <30537851+hiiri@users.noreply.github.com>
2020-03-26 15:27:21 +0200

    Update formatting and add python path

--- chall01/alcohen.py
+++ chall01/alcohen.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import string
 import sys
 
@@ -29,16 +30,16 @@ c = """.-
 --.."""
 string_alpha_or_spaces = all(x.isalpha() or x.isspace() for x in sys.argv[1])
 if (len(sys.argv) != 2 or not string_alpha_or_spaces or not sys.argv[1]):
-        print("usage: ./alcohen.py <a-zA-Z string>")
-            exit()
-            codes = c.split()
-            alph = list(string.ascii_lowercase)
-            d = dict(zip(alph, codes))
+    print("usage: ./alcohen.py <a-zA-Z string>")
+    exit()
+codes = c.split()
+alph = list(string.ascii_lowercase)
+d = dict(zip(alph, codes))
 
 
-            for x in str(sys.argv[1]).lower():
-                    if (x == " "):
-                                print(" ", end="")
-                                    else:
-                                                print(d[x], end="")
-                                                print()
+for x in str(sys.argv[1]).lower():
+    if (x == " "):
+        print(" ", end="")
+    else:
+        print(d[x], end="")
+print()

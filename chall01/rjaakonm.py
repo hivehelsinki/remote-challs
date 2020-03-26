cc4feec8d19d93a18f9a0293c323837b5308866d
rasmusjaa <rasmus@lense.fi>
2020-03-26 22:12:01 +0200

    changed order to create empty string only if there is 1 valid argv

--- chall01/rjaakonm.py
+++ chall01/rjaakonm.py
@@ -30,10 +30,10 @@ morse = [".-",
 	"--.."]
 
 def main():
-	str = ""
 	if (len(sys.argv) != 2) or (len(sys.argv[1]) == 0):
 		print("usage: ./rjaakonm.py <a-zA-Z string>")
 		return
+	str = ""
 	sys.argv[1] = sys.argv[1].lower()
 	for char in sys.argv[1]:
 		if char.isalpha():

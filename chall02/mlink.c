649283b36869d13aebc64c24f7e69e88f7b5e729
Maria Link <maria8link@gmail.com>
2020-03-30 17:02:19 +0300

    fixed mistake - didn't work correctly with digits less than 17

--- chall02/mlink.c
+++ chall02/mlink.c
@@ -11,14 +11,26 @@ char *hv_rgb2hex(int r, int g, int b)
     str = (char*)malloc(sizeof(char) * 8);
     str[7] = '\0';
     str[0] = '#';
-    str[1] = hex[r % 16];
-    r /= 16;
+    if (r > 16){
+        str[1] = hex[r % 16];
+        r /= 16;
+    }
+    else
+        str[1] = '0';
     str[2] = hex[r % 16];
-    str[3] = hex[g % 16];
-    g /= 16;
+    if (g > 16){
+        str[3] = hex[g % 16];
+        g /= 16;
+    }
+    else
+        str[3] = '0';
     str[4] = hex[g % 16];
-    str[5] = hex[b % 16];
-    b /= 16;
+    if (b > 16){
+        str[5] = hex[b % 16];
+        b /= 16;
+    }
+    else
+        str[5] = '0';
     str[6] = hex[b % 16];
     return (str);
 }

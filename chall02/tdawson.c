206504f5767fecfba64105c6967555d0a3af8e8b
tdawson-42 <tdawson@student.hive.fi>
2020-03-30 14:30:47 +0300

    fix formatting

--- chall02/tdawson.c
+++ chall02/tdawson.c
@@ -7,7 +7,6 @@ char *hv_rgb2hex(int r, int g, int b)
 
 	if (r < 0 || g < 0 || b < 0)
 		return NULL;
-
 	hex_str = malloc(8);
 	unsigned int hex = ((r & 0xff) << 16) + ((g & 0xff) << 8) + (b & 0xff);
 	sprintf(hex_str, "#%x", hex);

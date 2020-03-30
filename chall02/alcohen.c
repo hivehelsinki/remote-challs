f15228a7b0269f1c6e221adccb8e9501522f84c6
alcohen@student.hive.fi <alcohen@student.hive.fi>
2020-03-30 15:28:56 +0300

    Reduced repetition and split code into functions

--- chall02/alcohen.c
+++ chall02/alcohen.c
@@ -1,22 +1,31 @@
-char *hv_rgb2hex(int r, int g, int b)
+#include <stdlib.h>
+
+char    *color_to_hex(int color)
 {
-    char *s;
     char *base16 = "0123456789abcdef";
-    int i;
+    char *s;
+    int idx;
 
-    i = 0;
-
-    s = (char *)malloc(sizeof(char) * 8);
+    if (!(s = (char *)malloc(sizeof(char) * 8)))
+        return (NULL);  
+    idx = 6;
+    s[idx + 1] = '\0';
+    while (idx > 0)
+    {
+        s[idx] = base16[color % 16];
+        color /= 16;
+        idx--;
+    }
     s[0] = '#';
-    s[2] = base16[r % 16];
-    r /= 16;
-    s[1] = base16[r % 16];
-    s[4] = base16[g % 16];
-    g /= 16;
-    s[3] = base16[g % 16];
-    s[6] = base16[b % 16];
-    b /= 16;
-    s[5] = base16[b % 16];
-    s[7] = '\0';
     return (s);
+}
+
+char    *hv_rgb2hex(int r, int g, int b)
+{
+    int color;
+
+    if (r < 0 || r > 255 || g < 0 || g > 255 || b < 0 || b > 255)
+        return (NULL);
+    color = (r * 256 * 256) + (g * 256) + b;
+    return (color_to_hex(color));
 }

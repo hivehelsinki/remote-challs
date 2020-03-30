55fc19acf01660b039a35a62960988a418a9bbb7
Jony Salmi <jony.salmi97@gmail.com>
2020-03-30 16:11:41 +0300

    small adjustementos

--- chall02/jsalmi.c
+++ chall02/jsalmi.c
@@ -24,12 +24,12 @@ char *hv_rgb2hex(int r, int g, int b)
 
 	final = (char *)malloc(sizeof(char) * 8);
 	final[0] = '#';
-	final[7] = '\0';
 	final[1] = puthex((unsigned char)r / 16);
 	final[2] = puthex((unsigned char)r % 16);
 	final[3] = puthex((unsigned char)g / 16);
 	final[4] = puthex((unsigned char)g % 16);
 	final[5] = puthex((unsigned char)b / 16);
 	final[6] = puthex((unsigned char)b % 16);
+	final[7] = '\0';
 	return (final);
 }

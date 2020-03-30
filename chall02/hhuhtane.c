1f9b3c402f8da289ac5648b316808faa8f93d02b
hhuhtane <hhuhtane@student.hive.fi>
2020-03-30 21:09:41 +0300

    Started everything from the scratch. Now it is working properly.

--- chall02/hhuhtane.c
+++ chall02/hhuhtane.c
@@ -1,93 +1,39 @@
-#include <string.h>
-#include <stdio.h>
+#include <stdlib.h>
 
-size_t		hv_strlen(char str)
+static void		int2hex(int color, char *str)
 {
-	size_t	i;
+	int		i1;
+	int		i2;
+	int		color2;
 
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
-}
-
-size_t		hv_intlen_base(int n, int base)
-{
-	size_t		len;
-
-	len = 1;
-	if (n < 0)
-		len++;
-	while (n >= base || n <= -base)
+	i1 = 0;
+	i2 = 1;
+	while (str[i1])
+		i1++;
+	while (i2 >= 0)
 	{
-		n /= base;
-		len++;
-	}
-	return (len);
-}
-
-char	*hv_itoa_base(int n, int base)
-{
-	size_t		n_len;
-	size_t		temp;
-	char		str;
-
-	n_len = hv_intlen_base(n, base);
-	if ((str = (char*)malloc(sizeof(char) * n_len)))
-		return (NULL);
-	str[n_len--] = '\0';
-	while (n >= base || n <= -base)
-	{
-		temp = n % base;
-		if (temp <= 9)
-			str[n_len--] = temp + '0';
+		color2 = color % 16;
+		if (color2 < 10)
+			str[i1 + i2] = color2 + '0';
 		else
-			str[n_len--] = temp - 10 + 'a';
-		n /= base;
+			str[i1 + i2] = color2 - 10 + 'a';
+		color = color / 16;
+		i2--;
 	}
-	if (temp <= 9)
-		str[n_len] = temp + '0';
-	else
-		str[n_len] = temp - 10 + 'a';
-	return (str);
 }
 
-char	*hv_strcat(char *s1, char *s2)
+char			*hv_rgb2hex(int r, int g, int b)
 {
-	size_t	i1;
-	size_t	i2;
+	char		*hex;
 
-	i1 = hv_strlen(s1);
-	i2 = 0;
-	while (s2)
-		s1[i1++] = s2[i2++];
-	s1[i1] = '\0';
-	return (s1);
-}
-
-char	*hv_rgb2hex(int r, int g, int b)
-{
-	char*	hex_str;
-	char*	color_tmp;
-
-	if (r < 0 | g < 0 | b < 0)
+	if (r < 0 || g < 0 || b < 0 || r > 255 || g > 255 || b > 255)
 		return (0);
-	if (r > 255 | g > 255 | b > 255)
+	if (!(hex = (char*)malloc(sizeof(char) * 8)))
 		return (0);
-	if ((hex_str = (char*)malloc(sizeof(char) * 8)))
-		return (0);
-	color_tmp = ft_itoa_base(r, 16);
-	hv_strcat(hex_str, color_tmp)
-	color_tmp = ft_itoa_base(g, 16);
-	hv_strcat(hex_str, color_tmp)
-	color_tmp = ft_itoa_base(b, 16);
-	hv_strcat(hex_str, color_tmp)
-	return (hex_str);
-}
-
-int		main(void)
-{
-	printf("%d\n", strcmp(hv_rgb2hex(252, 186, 3), "#fcba03"));
-
-	return (0);
+	hex[0] = '#';
+	hex[7] = '\0';
+	int2hex(r, hex);
+	int2hex(g, hex);
+	int2hex(b, hex);
+	return (hex);
 }

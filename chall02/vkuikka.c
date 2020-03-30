bf0f9d251e78c7a13d71433287917a31aca50039
vkuikka <villekuikka1@gmail.com>
2020-03-30 16:51:19 +0300

    Added a memory allocation check for strdup.

--- chall02/vkuikka.c
+++ chall02/vkuikka.c
@@ -15,9 +15,8 @@ char			*hv_rgb2hex(int r, int g, int b)
 
 	if (r < 0 || r > 255 ||
 		g < 0 || g > 255 ||
-		b < 0 || b > 255)
-		return (NULL);
-	if (!(res = strdup("#000000")))
+		b < 0 || b > 255 ||
+		!(res = strdup("#000000")))
 		return (NULL);
 	ft_hex_to_str(res + 1, r);
 	ft_hex_to_str(res + 3, g);

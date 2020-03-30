62baafa13516ae7ad9e608b3b1110336c769d185
vkuikka <villekuikka1@gmail.com>
2020-03-30 15:00:28 +0300

    added vkuikka.c

---
+++ chall02/vkuikka.c
@@ -0,0 +1,20 @@
+#include <string.h>
+
+void	ft_hex_to_str(char *str, int num)
+{
+	char *hex = "0123456789abcdef";
+
+	str[1] = hex[num % 16];
+	str[0] = hex[num / 16 % 16];
+}
+
+char *hv_rgb2hex(int r, int g, int b)
+{
+	char	*res;
+
+	res = strdup("#000000");
+	ft_hex_to_str(res + 1, r);
+	ft_hex_to_str(res + 3, g);
+	ft_hex_to_str(res + 5, b);
+	return (res);
+}

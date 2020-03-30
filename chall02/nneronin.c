b8b41e63525f53f21c8a4384b06af80601a58bc8
Epicurius <niklas0702@gmail.com>
2020-03-30 16:27:45 +0300

    added failsafe for values >255 && <0

--- chall02/nneronin.c
+++ chall02/nneronin.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int	check(int value)
+{
+	return ((value < 0 || value > 255) ? 0 : 1);
+}
+
 char	*hv_rgb2hex(int r, int g, int b)
 {
 	char *str;
+	if (check(r) == 0 || check(g) == 0 || check(b) == 0)
+		return (NULL);
 	if (!(str = (char *)malloc(sizeof(char) * 8)))
 		return (NULL);
-	str[0] = '#';
-	sprintf(&str[1], "%x", (r & 0xFF) << 16 | (g & 0xFF) << 8 | (b & 0xFF));
+	sprintf(&str[0], "#%x", (r & 0xFF) << 16 | (g & 0xFF) << 8 | (b & 0xFF));
 	str[7] == '\0';
 	return (str);
 }

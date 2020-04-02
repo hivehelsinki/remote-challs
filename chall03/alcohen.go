b094ef8d2a3f9d133c6981608557ca2a11fbfc93
hiiri <alcohen@student.hive.fi>
2020-04-02 15:59:31 +0300

    Add global variable for time to clear code up in future

--- chall03/alcohen.go
+++ chall03/alcohen.go
@@ -10,6 +10,8 @@ import (
 	"log"
 )
 
+var g_time_start = time.Now()
+
 func make_get_request(url string) string {
 	resp, err := http.Get(url)
 	body, err := ioutil.ReadAll(resp.Body)
@@ -25,20 +27,17 @@ func generate_hex_request_url(url string, id string, resp_hex string) string {
 }
 
 func main() {
-	start := time.Now()
+	
 	url := "https://chall03.hive.fi/"
-	elapsed := time.Since(start) / 1e6
+	elapsed := time.Since(g_time_start) / 1e6
 	fmt.Printf("%dms - GET %s\n", elapsed, url)
 	
 	body := make_get_request(url)
-	elapsed = time.Since(start) / 1e6
+	elapsed = time.Since(g_time_start) / 1e6
 	fmt.Printf("%dms - GET %s\n", elapsed, url)
 	fmt.Printf("\tanswer:%s\n", body)
 	
 	s := strings.Split(body, "=")
-	
-	//id, r, g, b := s[1], s[2], s[5], s[4]
-	//id, _ := strconv.Atoi(strings.Split(s[1], ",")[0])
 	id := strings.Split(s[1], ",")[0]
 	r, _ := strconv.Atoi(strings.Split(s[2], ",")[0])
 	g, _ := strconv.Atoi(strings.Split(s[3], ",")[0])
@@ -48,7 +47,7 @@ func main() {
 	req_url := generate_hex_request_url(url, id, resp_hex)
 	fmt.Printf("%dms - GET %s\n", elapsed, req_url)
 	body = make_get_request(req_url)
-	elapsed = time.Since(start) / 1e6
+	elapsed = time.Since(g_time_start) / 1e6
 	fmt.Printf("%dms - GET %s\n", elapsed, req_url)
 	fmt.Printf("\tanswer: %s\n", body)
 	fmt.Printf("%dms - DONE\n", elapsed)

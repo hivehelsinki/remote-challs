925d0f6ce523b1e2f18dea91840a5c847f0b438c
tdawson-42 <tdawson@student.hive.fi>
2020-04-02 14:44:36 +0300

    use log.Fatal for errors, some formatting fixes

--- chall03/tdawson.go
+++ chall03/tdawson.go
@@ -3,39 +3,37 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 )
 
 func httpGet() {
 	res, err := http.Get("https://chall03.hive.fi")
 	if err != nil {
-		fmt.Printf("ERROR\n")
+		log.Fatal(err)
 	}
 	body, err := ioutil.ReadAll(res.Body)
-	defer res.Body.Close()
-
+	res.Body.Close()
 	if err != nil {
-		fmt.Printf("ERROR\n")
+		log.Fatal(err)
 	}
 
-	fmt.Println(string(body))
-	var id int
-	var r int
-	var g int
-	var b int
+	var id, r, g, b int
 	n, err := fmt.Sscanf(string(body), "id=%d,r=%d,g=%d,b=%d - Send your response here: chall03.hive.fi/?id=<id>&resp=<hex>", &id, &r, &g, &b)
 	if err != nil || n != 4 {
-		fmt.Printf("ERROR\n")
+		log.Fatal(err)
 	}
-	fmt.Printf("id:%d r:%d g:%d b:%d\n", id, r, g, b)
 	response := fmt.Sprintf("https://chall03.hive.fi/?id=%d&resp=%02x%02x%02x", id, r, g, b)
 	fmt.Println(response)
 	res, err = http.Get(response)
 	if err != nil {
-		fmt.Printf("ERROR\n")
+		log.Fatal(err)
 	}
-	defer res.Body.Close()
 	body, err = ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println(string(body))
 }
 

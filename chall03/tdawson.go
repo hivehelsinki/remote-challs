85809daa79c80bb38f6c23bb8171209ab3930b67
tdawson-42 <tdawson@student.hive.fi>
2020-04-02 15:19:21 +0300

    some refactoring

--- chall03/tdawson.go
+++ chall03/tdawson.go
@@ -7,8 +7,8 @@ import (
 	"net/http"
 )
 
-func httpGet() {
-	res, err := http.Get("https://chall03.hive.fi")
+func httpGet(url string) (bodyStr string) {
+	res, err := http.Get(url)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -17,26 +17,23 @@ func httpGet() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Println(string(body))
+	return string(body)
+}
 
+func parseBody(body string) (response string) {
 	var id, r, g, b int
-	n, err := fmt.Sscanf(string(body), "id=%d,r=%d,g=%d,b=%d - Send your response here: chall03.hive.fi/?id=<id>&resp=<hex>", &id, &r, &g, &b)
+	n, err := fmt.Sscanf(body, "id=%d,r=%d,g=%d,b=%d - Send your response here: chall03.hive.fi/?id=<id>&resp=<hex>", &id, &r, &g, &b)
 	if err != nil || n != 4 {
 		log.Fatal(err)
 	}
-	response := fmt.Sprintf("https://chall03.hive.fi/?id=%d&resp=%02x%02x%02x", id, r, g, b)
+	response = fmt.Sprintf("https://chall03.hive.fi/?id=%d&resp=%02x%02x%02x", id, r, g, b)
 	fmt.Println(response)
-	res, err = http.Get(response)
-	if err != nil {
-		log.Fatal(err)
-	}
-	body, err = ioutil.ReadAll(res.Body)
-	res.Body.Close()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(string(body))
+	return response
 }
 
 func main() {
-	httpGet()
+	url := "https://chall03.hive.fi/"
+	response := parseBody(httpGet(url))
+	httpGet(response)
 }

597e42e82d3a8683e3e62c15414744733e317c75
Niklas Pimenoff <npimenof@mpro.local>
2020-04-02 15:04:30 +0300

    added error check

--- chall03/npimenof.go
+++ chall03/npimenof.go
@@ -23,9 +23,12 @@ func main() {
 	id := string(body)[0:strings.Index(string(body), ",")]
 	rgb := string(body)[strings.Index(string(body), ",")+1 : strings.Index(string(body), " ")]
 	rgb_separated := strings.Split(rgb, ",")
-	r, _ := strconv.Atoi(rgb_separated[0][2:])
-	g, _ := strconv.Atoi(rgb_separated[1][2:])
-	b, _ := strconv.Atoi(rgb_separated[2][2:])
+	r, err := strconv.Atoi(rgb_separated[0][2:])
+	g, err := strconv.Atoi(rgb_separated[1][2:])
+	b, err := strconv.Atoi(rgb_separated[2][2:])
+	if err != nil {
+		log.Fatal(err)
+	}
 	hex := fmt.Sprintf("%02x%02x%02x", r, g, b)
 	res, err = client.Get("https://chall03.hive.fi/?" + id + "&resp=" + hex)
 	if err != nil {

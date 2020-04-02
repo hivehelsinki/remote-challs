02aa7dca8987edb251b40d81bf954b1a94a99ae0
hakolao <okkohakola@gmail.com>
2020-04-02 20:07:08 +0300

    pure golang approach

--- chall03/ohakola.go
+++ chall03/ohakola.go
@@ -1,35 +1,31 @@
 package main
 
-//#include<../chall02/ohakola.c>
-//#include<stdlib.h>
-//#include <ctype.h>
-// char	*parse_rgb_int(char *str, int *nb)
-// {
-// 	while (!isdigit(*str))
-// 		str++;
-// 	*nb = atoi(str);
-// 	while (isdigit(*str))
-// 		str++;
-//	return str;
-// }
-// char 	*body_to_hex(char *str, int *id) {
-//	char *res;
-// 	int r, g, b;
-//
-// 	str = parse_rgb_int(str, id);
-// 	str = parse_rgb_int(str, &r);
-// 	str = parse_rgb_int(str, &g);
-// 	str = parse_rgb_int(str, &b);
-//  res = hv_rgb2hex(r, g, b);
-// 	return res + 1;
-// }
-import "C"
 import (
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 )
+
+func parse_rgb_response_into_url(body string) string {
+	var id, r, g, b int
+	parser, err := fmt.Sscanf(
+		string(body),
+		"id=%d,r=%d,g=%d,b=%d - Send your response here: chall03.hive.fi/?id=<id>&resp=<hex>",
+		&id,
+		&r,
+		&g,
+		&b,
+	)
+	if parser != 4 || err != nil {
+		log.Fatal(err)
+	}
+	return fmt.Sprintf(
+		"https://chall03.hive.fi/?id=%d&resp=%s",
+		id,
+		fmt.Sprintf("%02x%02x%02x", r, g, b),
+	)
+}
 
 func http_get(client http.Client, url string) (string, bool) {
 	log.Println("Getting: " + url)
@@ -63,12 +59,7 @@ func main() {
 	if !ok {
 		log.Fatal("Failed to request rgb")
 	}
-	id := C.int(0)
-	hexa_url := fmt.Sprintf(
-		"https://chall03.hive.fi/?id=%d&resp=%s",
-		id,
-		C.GoString(C.body_to_hex(C.CString(rgb_result), &id)),
-	)
+	hexa_url := parse_rgb_response_into_url(rgb_result)
 	hexa_result, ok := http_get(client, hexa_url)
 	if !ok {
 		log.Fatal("Failed to request hexa")

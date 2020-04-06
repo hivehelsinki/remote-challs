d6037087daff918c0aeb7caa5aafc06005d497db
llahti <llahti@student.hive.fi>
2020-04-06 19:47:10 +0300

    Add checking input, fix while loop

--- chall04/llahti.py
+++ chall04/llahti.py
@@ -5,7 +5,7 @@ def move(grid, n):
     done = True
     y = n - 1
     while y > 0:
-        x = n
+        x = n - 1
         while x >= 0:
             if grid[y][x] in [' '] and grid[y - 1][x] in ['.']:
                 grid[y] = grid[y][:x] + '.' + grid[y][x + 1:]
@@ -15,15 +15,33 @@ def move(grid, n):
         y -=  1
     return done
 
+def checkInput(line, n):
+    allowed = {'.',' ','#'}
+    if not set(line).issubset(allowed):
+        return False
+    if len(line) != n:
+        return False
+    return True
+
 def main():
     input = sys.stdin.readlines()
     n = int(input[0])
-    grid = input[1 : n + 1]
+    grid = input[1:]
+
+    if len(grid) != n:
+        exit()
+
+    for line in grid:
+        line = line.rstrip('\n')
+        if not checkInput(line, n):
+            exit()
+
     done = False
     while not done:
         done = move(grid, n)
+
     for line in grid:
-        print(line.rstrip())
+        print(line.rstrip('\n'))
 
 if __name__ == "__main__":
     main()

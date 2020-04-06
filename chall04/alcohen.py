77eeda19b2f5e9cd3157ecdb577549f40e9cb5c6
hiiri <alcohen@student.hive.fi>
2020-04-06 14:46:30 +0300

    Add 'moved' variable back to check if anything needs to be moved

--- chall04/alcohen.py
+++ chall04/alcohen.py
@@ -11,16 +11,23 @@ def can_move(next_under):
         return True
     return False
 
-
-
-for row in range(size):
-    for col in range(size):
-        if (grid[row][col] == '.'):
-            if (row + 1 < size and can_move(grid[row + 1][col])):
-                grid[row][col] = ' '
-                grid[row + 1][col] = '.'
+moved = True
+while moved:
+    moved = False
+    for row in range(size):
+        for col in range(size):
+            if (grid[row][col] == '.'):
+                if (row + 1 < size and can_move(grid[row + 1][col])):
+                    grid[row][col] = ' '
+                    grid[row + 1][col] = '.'
+                    moved = True
 
 for i in grid:
     for j in i:
         print(j, end="")
     print()
+
+
+
+          
+

cbc258e2568d4ca2d792f4cf53e5e5af1b913261
rasmusjaa <rasmus@lense.fi>
2020-04-06 16:40:58 +0300

    speed optimizations: added count variable so there's no need to check len of list. Also made one statement to elif instead of if and moved it after first if to make it possible to skip it

--- chall04/rjaakonm.py
+++ chall04/rjaakonm.py
@@ -4,23 +4,25 @@ import sys
 
 def main():
 	lines = []
+	counted = 0
 	for line in sys.stdin:
 		lines.append(list(line))
+		counted += 1
 	lines[0] ="".join(lines[0])
 	try:
 		size = int(lines[0])
 	except:
 		return
-	if size == len(lines) - 1:
+	if size == counted - 1:
 		i = 1
 		while i < size:
 			j = 0
 			while j < size:
-				if lines[i][j] != '.' and lines[i][j] != '#' and lines[i][j] != ' ':
-					return
 				if lines[i][j] == '.' and lines[i + 1][j] == ' ':
 					lines[i][j] = ' '
 					lines[i + 1][j] = '.'
+				elif lines[i][j] != '.' and lines[i][j] != '#' and lines[i][j] != ' ':
+					return
 				j += 1
 			if lines[i][j] != '\n' and lines[i][j] != '\0':
 				return

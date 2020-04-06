8c4f75986b02aa70acf79e3342de35dde2d07bd2
sadawi <hahli144@hotmail.com>
2020-04-06 14:14:01 +0300

    sadawi.py

---
+++ chall04/sadawi.py
@@ -0,0 +1,23 @@
+#! /usr/bin/python3
+
+i = int(input())
+
+arr = []
+for j in range(i):
+	arr.append(list(input()))
+
+flag = 1
+while flag:
+	flag = 0
+	for j in range(3, -1, -1):
+		for k, c in enumerate(arr[j]):
+			if c == '.':
+				if arr[j+1][k] == ' ':
+					arr[j][k] = ' '
+					arr[j+1][k] = '.'
+					flag = 1
+
+for lst in arr:
+	for c in lst:
+		print(c, end="")
+	print()

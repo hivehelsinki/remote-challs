534e448363b0ae74f58926c557cf7b06ec6b3515
tdawson-42 <tdawson@student.hive.fi>
2020-04-06 16:16:17 +0300

    refactoring, simulation is now recursive

--- chall04/tdawson.py
+++ chall04/tdawson.py
@@ -6,6 +6,17 @@ import sys
 def error(error_msg):
     sys.exit('Error: ' + error_msg)
 
+def move_sand(grid, row, col):
+    grid[row][col] = ' '
+    grid[row+1][col] = '.'
+
+def simulate(grid):
+    for row in range(size - 2, -1, -1):
+        for col, c in enumerate(grid[row]):
+            if (c is '.' and grid[row+1][col] is ' '):
+                move_sand(grid, row, col)
+                simulate(grid)
+
 size = input()
 if not (size.isdigit()):
     error('Not a positive integer.')
@@ -15,19 +26,12 @@ grid = []
 for _ in range(size):
     row = input()
     if len(row) != size:
-        error('Input length different than size.')
+        error('Input length is different than size.')
     if not all(c in ' #.' for c in row):
         error('Forbidden characters in input.')
     grid.append(list(row))
 
-settled = False
-while (not settled):
-    settled = True
-    for row in range(size - 2, -1, -1):
-        for col, c in enumerate(grid[row]):
-            if (c is '.' and grid[row+1][col] is ' '):
-                settled = False
-                grid[row+1][col], grid[row][col] = grid[row][col], grid[row+1][col]
+simulate(grid)
 
 for row in grid:
     print(''.join(row))

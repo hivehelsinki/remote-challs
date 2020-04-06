1c8160ec675e6cf99acc312add1271d92ebf52d1
Don-Hege <heikki-seta@hotmail.com>
2020-04-07 01:00:24 +0300

    made it little bit nicer

--- chall04/vtran.py
+++ chall04/vtran.py
@@ -3,35 +3,50 @@
 import re
 import sys
 
-siz = int(input())
-arr = []
-i = 0
-while i < siz:
-    arr.append(input())
-    i += 1
-i = siz - 1
-while i >= 0:
-    if (len(arr[i]) != siz) :
-        print("input not a grid")
+def checkIfValidinput(siz, arr):
+    i = siz - 1
+    while i >= 0:
+        if (len(arr[i]) != siz) :
+            print("input not a grid")
+            sys.exit()
+        i -= 1
+    listToStr = returnListToStr(arr)
+    maximum = str(siz * siz)
+    regex = "[. #]{" + maximum + "}"
+    if not re.match(regex, listToStr) :
+        print("usage: [. #]*")
         sys.exit()
-    i -= 1
-listToStr = ''.join(map(str, arr))
-maximum = str(siz * siz)
-regex = "[. #]{" + maximum + "}"
-if not re.match(regex, listToStr) :
-    print("usage: [. #]*")
-    sys.exit()
-StrToList = list(listToStr)
-i = 0
-allcellsbutnotlastcolumns = siz * siz - siz
-while (i < allcellsbutnotlastcolumns):
-    if (StrToList[i] == '.' and StrToList[i + siz] == ' '):
-        StrToList[i] = ' '
-        StrToList[i + siz] = '.'
-        i = 0
-    i += 1
-i = 0
-while (i < siz):
-    StrToList[i : i + siz] = [''.join(StrToList[i : i + siz])]
-    print(StrToList[i])
-    i += 1
+
+def returnListToStr(arr):
+    return ''.join(map(str, arr))
+
+def droppingDots(listToStr, siz):
+    StrToList = list(listToStr)
+    i = 0
+    allcellsbutnotlastcolumns = siz * siz - siz
+    while (i < allcellsbutnotlastcolumns):
+        if (StrToList[i] == '.' and StrToList[i + siz] == ' '):
+            StrToList[i] = ' '
+            StrToList[i + siz] = '.'
+            i = 0
+        i += 1
+    return StrToList
+
+def printter(StrToList, siz):
+    i = 0
+    while (i < siz):
+        StrToList[i : i + siz] = [''.join(StrToList[i : i + siz])]
+        print(StrToList[i])
+        i += 1
+
+def main ():
+    siz = int(input())
+    arr = []
+    i = 0
+    while i < siz:
+        arr.append(input())
+        i += 1
+    checkIfValidinput(siz, arr)
+    done = droppingDots(returnListToStr(arr), siz)
+    printter(done, siz)
+main()

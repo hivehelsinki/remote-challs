9c2ecaa88ece19628d3c88c1376e0b735554ba69
hakolao <okkohakola@gmail.com>
2020-04-06 17:58:49 +0300

    Fix buggety bugs

--- chall04/ohakola.py
+++ chall04/ohakola.py
@@ -6,7 +6,7 @@
 #    By: ohakola <ohakola@student.hive.fi>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/04/06 17:30:11 by ohakola           #+#    #+#              #
-#    Updated: 2020/04/06 17:47:59 by ohakola          ###   ########.fr        #
+#    Updated: 2020/04/06 17:58:25 by ohakola          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -16,13 +16,17 @@ import sys
 # Simulates sand fall of given grid array where
 # each sand piece falls as long as there's empty below it
 def sand_fall(grid, row_size):
-	for y in range(row_size):
-		for x in range(row_size):
-			if y < 4:
-				curr_i = y * row_size + x
-				nxt_i = curr_i + row_size
-				if grid[curr_i] == "." and grid[nxt_i] == " ":
-					grid[curr_i], grid[nxt_i] = grid[nxt_i], grid[curr_i]
+	changed = True
+	while changed:
+		changed = False
+		for y in range(row_size):
+			for x in range(row_size):
+				if y < row_size - 1:
+					curr_i = y * row_size + x
+					nxt_i = curr_i + row_size
+					if grid[curr_i] == "." and grid[nxt_i] == " ":
+						grid[curr_i], grid[nxt_i] = grid[nxt_i], grid[curr_i]
+						changed = True
 	return grid
 
 # prints grid string of given size
@@ -34,7 +38,11 @@ def print_grid(grid, size):
 
 if __name__ == "__main__":
 	lines = sys.stdin.readlines()
-	row_size = int(lines.pop(0))
+	try:
+		row_size = int(lines.pop(0))
+	except:
+		print("Wrong size given")
+		exit()
 	for index, line in enumerate(lines) :
   		lines[index] = line.rstrip("\n")
 	initial_grid = list("".join(lines))

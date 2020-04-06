7d84d53cae5251ab7629c766d164f943d0a6264f
hiiri <alcohen@student.hive.fi>
2020-04-06 14:39:33 +0300

    Remove useless variable

--- chall04/alcohen.py
+++ chall04/alcohen.py
@@ -11,15 +11,14 @@ def can_move(next_under):
         return True
     return False
 
-moved = True
-while moved:
-    moved = False
-    for row in range(size):
-        for col in range(size):
-            if (grid[row][col] == '.'):
-                if (row + 1 < size and can_move(grid[row + 1][col])):
-                    grid[row][col] = ' '
-                    grid[row + 1][col] = '.'
+
+
+for row in range(size):
+    for col in range(size):
+        if (grid[row][col] == '.'):
+            if (row + 1 < size and can_move(grid[row + 1][col])):
+                grid[row][col] = ' '
+                grid[row + 1][col] = '.'
 
 for i in grid:
     for j in i:

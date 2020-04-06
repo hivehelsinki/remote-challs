0d280d46a5e57ead4de3156eb011145b5953b488
gmolin <gmolin@student.hive.fi>
2020-04-06 19:29:38 +0300

    added more edge case protection for reading N and fixed output printing (now without newlines)

--- chall04/gmolin.py
+++ chall04/gmolin.py
@@ -6,10 +6,12 @@ def main():
 	board = []
 	for line in sys.stdin:
 		board.append(list(line))
-	board_size = int
-	for i in board[0]:
-		if i.isdigit():
-			board_size = int(i)
+	board[0] ="".join(board[0])
+	try:
+		board_size = int(board[0].rstrip())
+	except:
+		print ("Error: N is not a digit")
+		return
 	if board_size != len(board) - 1:
 		print("Error: Invalid map size")
 		return
@@ -26,16 +28,19 @@ def main():
 					return
 				try:
 					if (board[i][j] == "." and board[i + 1][j] == " "):
-						board[i + 1][j] = "."
-						board[i][j] = " "
+						board[i + 1][j] = '.'
+						board[i][j] = ' '
 				except:
 					print("Error: Invalid file")
 					return
 				j += 1
+			board[i]= "".join(board[i])
 			i += 1
+		board[board_size]= "".join(board[board_size])
 		i = 1
 		while (i <= board_size):
-			print(board[i])
+			print(board[i], end = "")
 			i += 1
+
 if __name__ == "__main__":
 	main()

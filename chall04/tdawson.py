48fb67bf53df4cd8e75c99194d0c09f09b2fb4f0
tdawson-42 <tdawson@student.hive.fi>
2020-04-06 17:17:39 +0300

    more refactoring

--- chall04/tdawson.py
+++ chall04/tdawson.py
@@ -3,32 +3,24 @@
 
 import sys
 
-def error(error_msg):
-    sys.exit('Error: ' + error_msg)
-
-def move_sand(grid, row, col):
-    grid[row][col] = ' '
-    grid[row+1][col] = '.'
-
 def simulate(grid):
     for row in range(size - 2, -1, -1):
         for col, c in enumerate(grid[row]):
             if (c is '.' and grid[row+1][col] is ' '):
-                move_sand(grid, row, col)
+                grid[row][col] = ' '
+                grid[row+1][col] = '.'
                 simulate(grid)
 
 size = input()
 if not (size.isdigit()):
-    error('Not a positive integer.')
+    sys.exit('Error: Not a positive integer.')
 size = int(size)
 
 grid = []
 for _ in range(size):
     row = input()
-    if len(row) != size:
-        error('Input length is different than size.')
-    if not all(c in ' #.' for c in row):
-        error('Forbidden characters in input.')
+    if len(row) != size or not all(c in ' #.' for c in row):
+        sys.exit('Error: Input row is invalid.')
     grid.append(list(row))
 
 simulate(grid)

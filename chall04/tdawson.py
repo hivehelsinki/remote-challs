74e91060b99a5c1e7e9e071e93b9acd1322edeec
tdawson-42 <tdawson@student.hive.fi>
2020-04-06 15:34:34 +0300

    add error checks, renamed variables

--- chall04/tdawson.py
+++ chall04/tdawson.py
@@ -1,23 +1,33 @@
 #!/usr/bin/python3
-#Python 3.6.9
+# Python 3.6.9
 
 import sys
 
-n = int(input())
-grid = []
+def error(error_msg):
+    sys.exit('Error: ' + error_msg)
 
-for _ in range (n):
-    grid.append(list(input()))
+size = input()
+if not (size.isdigit()):
+    error('Not a positive integer.')
+size = int(size)
+
+grid = []
+for _ in range(size):
+    row = input()
+    if len(row) != size:
+        error('Input length different than size.')
+    if not all(c in ' #.' for c in row):
+        error('Forbidden characters in input.')
+    grid.append(list(row))
 
 settled = False
 while (not settled):
     settled = True
-    for i in range(n - 2, -1, -1):
-        for j, c in enumerate(grid[i]):
-            if (c is '.' and grid[i+1][j] is ' '):
+    for row in range(size - 2, -1, -1):
+        for col, c in enumerate(grid[row]):
+            if (c is '.' and grid[row+1][col] is ' '):
                 settled = False
-                grid[i+1][j], grid[i][j] = grid[i][j], grid[i+1][j]
-
+                grid[row+1][col], grid[row][col] = grid[row][col], grid[row+1][col]
 
 for row in grid:
     print(''.join(row))

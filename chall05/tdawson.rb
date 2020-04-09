3f3eb9d610b96ca212706294359f28fdca1d6b74
tdawson-42 <tdawson@student.hive.fi>
2020-04-09 21:33:07 +0300

    change to using css selectors, add check for no arguments given, refactoring

--- chall05/tdawson.rb
+++ chall05/tdawson.rb
@@ -5,12 +5,8 @@ require 'open-uri'
 require 'nokogiri'
 
 $WIKI_URL = String.new("https://en.wikipedia.org/wiki/")
-$seen_links = []
+$seen_pages = []
 $counter = 0
-
-def is_seen(page)
-	return $seen_links.include?(page)
-end
 
 def open_URL(url)
 	begin
@@ -21,10 +17,10 @@ def open_URL(url)
 end
 
 def next_wiki_link(links, i)
-	until links[i].to_s.match(/^\/wiki\/[^\/#.:]+$/) do
+	until links[i].to_s.match(/^\/wiki\/[^\/:]+$/) do
 		i += 1
 		if (i >= links.size)
-			abort("no valid Wikipedia link found!")
+			abort("Error: No valid Wikipedia link found!")
 		end
 	end
 	return i
@@ -32,23 +28,25 @@ end
 
 def find_philosophy(page)
 	doc = Nokogiri::HTML.parse(open_URL($WIKI_URL + page))
-	attrs = doc.xpath('//p//a/@href', '//li//a/@href')
-	attrs.map {|attr| attr.value}
-	i = next_wiki_link(attrs, 0)
-	page = attrs[i].to_s.split('/')[2]
+	links = doc.css('div.mw-content-ltr p a[href]', 'div.mw-content-ltr li a[href]').map {|element| element['href']}
+	i = next_wiki_link(links, 0)
+	page = links[i].to_s.split('/')[-1]
 	if page.eql?("Philosophy")
 		puts "!!! Reach Philosophy !!!"
 		exit
 	end
-	until not is_seen(page) do
+	until not $seen_pages.include?(page) do
 		puts "Skip #{page}, already seen this one"
-		i = next_wiki_link(attrs, i+1)
-		page = attrs[i].to_s.split('/')[2]
+		i = next_wiki_link(links, i+1)
+		page = links[i].to_s.split('/')[-1]
 	end
-	$seen_links << page
+	$seen_pages << page
 	$counter += 1
 	puts "\tGoing to #{page} (counter: #{$counter})"
 	find_philosophy(page)
 end
 
+if ARGV.empty? or ARGV[0].empty?
+	abort("Error: No argument given!")
+end
 find_philosophy(ARGV[0])

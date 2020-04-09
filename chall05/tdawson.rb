61740152c5f2cf908dac46b71f624157f2aec6f0
tdawson-42 <tdawson@student.hive.fi>
2020-04-09 23:03:33 +0300

    seen pages are put in a set instead of array, iterate through links without using the index

--- chall05/tdawson.rb
+++ chall05/tdawson.rb
@@ -3,50 +3,41 @@
 
 require 'open-uri'
 require 'nokogiri'
+require 'set'
 
 $WIKI_URL = String.new("https://en.wikipedia.org/wiki/")
-$seen_pages = []
-$counter = 0
+$seen_pages = Set.new
 
 def open_URL(url)
-	begin
-		file = open(url)
-	rescue
-		abort("no wikipedia page for this word!")
-	end
+	file = open(url) rescue abort("no wikipedia page for this word!")
 end
 
-def next_wiki_link(links, i)
-	until links[i].to_s.match(/^\/wiki\/[^\/:]+$/) do
-		i += 1
-		if (i >= links.size)
-			abort("Error: No valid Wikipedia link found!")
+def next_wiki_page(links)
+	links.each { |link|
+		if link.match(/^\/wiki\/[^\/:]+$/) 
+			page = link.split('/')[-1]
+			if $seen_pages.include?(page)
+				puts "Skip #{page}, already seen this one"
+				next
+			end
+			return page
 		end
-	end
-	return i
-end	
+	}
+	abort("No valid wikipedia page link found!")
+end
 
-def find_philosophy(page)
-	doc = Nokogiri::HTML.parse(open_URL($WIKI_URL + page))
-	links = doc.css('div.mw-content-ltr p a[href]', 'div.mw-content-ltr li a[href]').map {|element| element['href']}
-	i = next_wiki_link(links, 0)
-	page = links[i].to_s.split('/')[-1]
+def find_philosophy(page, counter)
+	doc = Nokogiri::HTML.parse(open_URL("#{$WIKI_URL}#{page}"))
+	links = doc.css('div.mw-content-ltr p a[href]', 'div.mw-content-ltr li a[href]').map { |element| element['href'] }
+	page = next_wiki_page(links)
 	if page.eql?("Philosophy")
-		puts "!!! Reach Philosophy !!!"
-		exit
+		puts "!!! Reach Philosophy !!!"; exit
 	end
-	until not $seen_pages.include?(page) do
-		puts "Skip #{page}, already seen this one"
-		i = next_wiki_link(links, i+1)
-		page = links[i].to_s.split('/')[-1]
-	end
-	$seen_pages << page
-	$counter += 1
-	puts "\tGoing to #{page} (counter: #{$counter})"
-	find_philosophy(page)
+	$seen_pages << page;
+	puts "\tGoing to #{page} (counter: #{counter})"
+	find_philosophy(page, counter += 1)
 end
 
-if ARGV.empty? or ARGV[0].empty?
-	abort("Error: No argument given!")
-end
-find_philosophy(ARGV[0])
+#if no argument is given, start from Wikipedia main page :D
+if ARGV.empty? then start = "" else start = ARGV[0] end
+find_philosophy(start, 1)

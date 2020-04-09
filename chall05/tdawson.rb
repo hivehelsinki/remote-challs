4b0c51eb384c5064dbfdc64adf766763614e3918
tdawson-42 <tdawson@student.hive.fi>
2020-04-09 20:23:38 +0300

    fixes and refactoring

--- chall05/tdawson.rb
+++ chall05/tdawson.rb
@@ -8,6 +8,10 @@ $WIKI_URL = String.new("https://en.wikipedia.org/wiki/")
 $seen_links = []
 $counter = 0
 
+def is_seen(page)
+	return $seen_links.include?(page)
+end
+
 def open_URL(url)
 	begin
 		file = open(url)
@@ -17,34 +21,34 @@ def open_URL(url)
 end
 
 def next_wiki_link(links, i)
-	until links[i].to_s.match(/^\/wiki\/.+$/) do
+	until links[i].to_s.match(/^\/wiki\/[^\/#.:]+$/) do
 		i += 1
+		if (i >= links.size)
+			abort("no valid Wikipedia link found!")
+		end
 	end
 	return i
 end	
 
-def find_philosophy(article)
-	doc = Nokogiri::HTML.parse(open_URL($WIKI_URL + article))
-	attrs = doc.xpath('//p//a/@href') 
+def find_philosophy(page)
+	doc = Nokogiri::HTML.parse(open_URL($WIKI_URL + page))
+	attrs = doc.xpath('//p//a/@href', '//li//a/@href')
 	attrs.map {|attr| attr.value}
-	i = 0
-	i = next_wiki_link(attrs, i)
-	article = attrs[i].to_s.split('/')[2]
-	if article.eql?("Philosophy")
+	i = next_wiki_link(attrs, 0)
+	page = attrs[i].to_s.split('/')[2]
+	if page.eql?("Philosophy")
 		puts "!!! Reach Philosophy !!!"
 		exit
 	end
-	if ($seen_links.include?(article))
-		puts "Skip #{article}, already seen this one"
-		i = next_wiki_link(attrs, i)
-		article = attrs[i].to_s.split('/')[2]
-	else
-		$seen_links << article
+	until not is_seen(page) do
+		puts "Skip #{page}, already seen this one"
+		i = next_wiki_link(attrs, i+1)
+		page = attrs[i].to_s.split('/')[2]
 	end
+	$seen_links << page
 	$counter += 1
-	puts "\tGoing to #{article} (counter: #{$counter})"
-	find_philosophy(article)
+	puts "\tGoing to #{page} (counter: #{$counter})"
+	find_philosophy(page)
 end
 
-start = ARGV[0]
-find_philosophy(start)
+find_philosophy(ARGV[0])

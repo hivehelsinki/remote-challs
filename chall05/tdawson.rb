704861a37cfdf7aa4d53e2f9d8c81e466c822465
tdawson-42 <tdawson@student.hive.fi>
2020-04-09 23:42:45 +0300

    more refactoring

--- chall05/tdawson.rb
+++ chall05/tdawson.rb
@@ -8,18 +8,18 @@ require 'set'
 $WIKI_URL = String.new("https://en.wikipedia.org/wiki/")
 $seen_pages = Set.new
 
-def open_URL(url)
-	file = open(url) rescue abort("no wikipedia page for this word!")
+def get_page_links(page)
+	file = open($WIKI_URL + page) rescue abort("no wikipedia page for this word!")
+	links = Nokogiri::HTML.parse(file)
+		.css('div.mw-content-ltr p a[href]', 'div.mw-content-ltr li a[href]')
+		.map { |elem| elem['href'] }
 end
 
 def next_wiki_page(links)
 	links.each { |link|
 		if link.match(/^\/wiki\/[^\/:]+$/) 
 			page = link.split('/')[-1]
-			if $seen_pages.include?(page)
-				puts "Skip #{page}, already seen this one"
-				next
-			end
+			(puts "Skip #{page}, already seen this one"; next) if $seen_pages.include?(page)
 			return page
 		end
 	}
@@ -27,17 +27,14 @@ def next_wiki_page(links)
 end
 
 def find_philosophy(page, counter)
-	doc = Nokogiri::HTML.parse(open_URL("#{$WIKI_URL}#{page}"))
-	links = doc.css('div.mw-content-ltr p a[href]', 'div.mw-content-ltr li a[href]').map { |element| element['href'] }
+	links = get_page_links(page)
 	page = next_wiki_page(links)
-	if page.eql?("Philosophy")
-		puts "!!! Reach Philosophy !!!"; exit
-	end
+	(puts "!!! Reach Philosophy !!!"; exit) if page.eql?("Philosophy")
 	$seen_pages << page;
 	puts "\tGoing to #{page} (counter: #{counter})"
 	find_philosophy(page, counter += 1)
 end
 
-#if no argument is given, start from Wikipedia main page :D
-if ARGV.empty? then start = "" else start = ARGV[0] end
+if ARGV.empty? then start = "" else start = ARGV[0] end #start from main page if no arg given :D
+(puts "!!! Reach Philosophy !!!"; exit) if ARGV[0].eql?("Philosophy")
 find_philosophy(start, 1)

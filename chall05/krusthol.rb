1ca22931c41fefc625416840ea616f550177c2ce
Kalle Rustholkarhu <krusthol@student.hive.fi>
2020-04-11 11:55:53 +0300

    Fix missing errorchecks, missing skipping page print

--- chall05/krusthol.rb
+++ chall05/krusthol.rb
@@ -4,7 +4,12 @@ require 'open-uri'
 
 counter = 0
 checked_pages = []
-starting_point = 'https://en.wikipedia.org/wiki/' + ARGV[0]
+if ARGV[0] != nil
+    starting_point = 'https://en.wikipedia.org/wiki/' + ARGV[0]
+else
+    puts "Road to Philosophy script usage: ./krusthol.rb [Wikipedia article]"
+    exit
+end
 begin
 contents = open(starting_point).read
 rescue
@@ -12,16 +17,27 @@ rescue
         exit
 end
 split_p = contents.split('<p>')
-road = split_p[1].scan(/(?<=<a href="\/wiki\/).[^"]*/)
+i = 1
+road = split_p[i].scan(/(?<=<a href="\/wiki\/).[^"]*/)
+while road[0] == nil do
+    i = i + 1
+    road = split_p[i].scan(/(?<=<a href="\/wiki\/).[^"]*/)    
+end
 checked_pages.push(road[0])
 counter = counter + 1
 puts ("Going to " + road[0] + " (counter: #{counter})")
 while !checked_pages.include?('Philosophy') do
     contents = open('https://en.wikipedia.org/wiki/' + checked_pages.last).read
-    split_open = contents.split('<p>')
-    road = split_open[1].scan(/(?<=<a href="\/wiki\/).[^"]*/)
+    split_p = contents.split('<p>')
+    i = 1
+    road = nil
+    while road == nil do
+        road = split_p[i].scan(/(?<=<a href="\/wiki\/).[^"]*/)
+        i = i + 1
+    end
     i = 0
     while checked_pages.include?(road[i]) do
+        puts ("Skip " + road[i] + ", already seen this one")
         i = i + 1    
     end
     checked_pages.push(road[i])

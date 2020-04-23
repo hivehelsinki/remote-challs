006c7f3772fa9d8cc0c88de6840f3cacf87d50c6
rasmusjaa <rasmus@lense.fi>
2020-04-23 22:56:57 +0300

    changed logic so that books cant be split

--- chall06/rjaakonm.py
+++ chall06/rjaakonm.py
@@ -3,45 +3,59 @@
 import sys
 import fileinput
 
-def solve(lines, name):
+def solve(lines, name, files):
 	if len(lines) < 1: 
 		print(sys.argv[0], ": ", name, ": Can't read file", sep = "")
 		return
 	try:
-		widths = [int(i) for i in lines[0].split()]
+		shelf_widths = [int(i) for i in lines[0].split()]
 	except:
 		print(sys.argv[0], ": ", name, ": Can't read file", sep = "")
 		return
-	widths.sort(reverse=True)
-	shelf_amount = len(widths)
+	shelf_widths.sort(reverse=True)
+	shelf_amount = len(shelf_widths)
 	if shelf_amount < 1: 
 		print(sys.argv[0], ": ", name, ": Can't read file", sep = "")
 		return
 	iterlines = iter(lines)
 	next(iterlines)
-	total_width = 0
+	book_widths = []
 	for line in iterlines:
 		book_width = line.split(' ')
 		if len(book_width) < 2 or book_width[0].isdigit() == 0: 
 			print(sys.argv[0], ": ", name, ": Can't read file", sep = "")
 			return
-		total_width += int(book_width[0])
-	needed = 0
-	shelf_width = 0
-	while total_width > shelf_width and needed < shelf_amount:
-		shelf_width += widths[needed]
-		needed += 1
-	if total_width > shelf_width:
-		print(sys.argv[0], ': ', name, ': Not enough space in the given shelves', sep = '')
-	else:
-		print (needed)
+		book_widths.append(int(book_width[0]))
+	if len(book_widths) == 0:
+			if files > 1:
+				print (name, ":", sep = "")
+			print ("0")
+			return
+	book_widths.sort(reverse=True)
+	for i in range(len(shelf_widths)):
+		books = len(book_widths)
+		j = 0
+		while j < books:
+			if shelf_widths[i] >= book_widths[j]:
+				shelf_widths[i] -= book_widths[j]
+				del book_widths[j]
+				j = 0
+				books -= 1
+			else:
+				j += 1
+		if len(book_widths) == 0:
+			if files > 1:
+				print (name, ":", sep = "")
+			print (i + 1)
+			return
+	print(sys.argv[0], ': ', name, ': Not enough space in the given shelves', sep = '')
 
 def main():	
 	lines = []
 	if len(sys.argv) == 1:
 		for line in fileinput.input():
 			lines.append(line)
-		solve(lines, 'stdin')
+		solve(lines, 'stdin', 1)
 	else:
 		iterargs = iter(sys.argv)
 		next(iterargs)
@@ -53,7 +67,7 @@ def main():
 			except:
 				print(sys.argv[0], ": ", arg, ": Can't read file", sep = "")
 			else:
-				solve(lines, arg)
+				solve(lines, arg, len(sys.argv) - 1)
 	
 
 if __name__ == "__main__":

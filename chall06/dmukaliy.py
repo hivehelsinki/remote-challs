2e8eb340fb1e2fc84132bf0ec8de5e7d68dba7c0
DianaMukaliyeva <diana.mukaliyeva@gmail.com>
2020-04-24 09:52:49 +0300

    last

--- chall06/dmukaliy.py
+++ chall06/dmukaliy.py
@@ -5,31 +5,46 @@ import sys
 def error(error_message, filename):
     print(f'{sys.argv[0]}: {filename}: {error_message}')
 
-def get_collections_size(data):
-    collections = []
+def get_sorted_books_width(data):
+    books = []
     for i in range(1, len(data)):
-        collection = data[i]
-        collections.append(int(collection.split()[0]))
-    return collections
+        book = data[i]
+        books.append(int(book.split()[0]))
+    books.sort(reverse=True)
+    return books
+
+def get_amount_shelves(shelves, books):
+    shelves_amount = -1
+    for i in range(0, len(books)):
+        found = False
+        for j in range(0, len(shelves)):
+            if shelves[j] - books[i] >= 0:
+                shelves[j] -= books[i]
+                found = True
+                break
+        if j > shelves_amount:
+            shelves_amount = j
+        if not found:
+            return -1
+    return shelves_amount + 1
 
 def parse_and_solve(data, filename):
     shelves = [int(shelf) for shelf in data[0].split()]
     if len(shelves) == 0:
         exit()
-    collections = get_collections_size(data)
-    shelves.sort(reverse=True)
-    unallocated_books_amount = sum(collections)
-    if unallocated_books_amount == 0:
+    books = get_sorted_books_width(data)
+    if sum(books) == 0:
         print('0')
         return
-    for i in range(len(shelves)):
-        unallocated_books_amount -= shelves[i]
-        if unallocated_books_amount <= 0:
-            break
-    if unallocated_books_amount > 0:
+    shelves.sort(reverse=True)
+    if shelves[0] < books[0]:
+        error('Not enough space in the given shelves', filename)
+        return
+    shelves_amount = get_amount_shelves(shelves, books)
+    if shelves_amount == -1:
         error('Not enough space in the given shelves', filename)
     else:
-        print(i + 1)
+        print(shelves_amount)
 
 def main():
     argc = len(sys.argv)

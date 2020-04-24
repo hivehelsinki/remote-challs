94be78fc9771ec8558c2ab0d7e94de881f7a8710
Kalle Rustholkarhu <krusthol@student.hive.fi>
2020-04-24 07:40:17 +0300

    Minimal refactoring and polish on small amount of code

--- chall06/krusthol.py
+++ chall06/krusthol.py
@@ -4,20 +4,20 @@ import sys
 READ_ERR = "Can't read file"
 FIT_ERR = "Not enough space in the given shelves"
 
-def proceed_with_error(many_args, stdin_mode, index, message):
+def print_proceed(many_args, std_input, index, message):
 	if many_args:
 		print(f"{sys.argv[index]}:")
-	if stdin_mode:
+	if std_input:
 		print(f"{sys.argv[0]}: stdin: {message}")
 	else:
 		print(f"{sys.argv[0]}: {sys.argv[index]}: {message}")
 	return (index + 1)
 
 def book_fitter(index):
-	many_args = True if len(sys.argv) > 2 else False
-	stdin_mode = True if len(sys.argv) == 1 else False
+	many_args = len(sys.argv) > 2
+	std_input = len(sys.argv) == 1
 	books = []
-	if stdin_mode:
+	if std_input:
 		lines = []
 		while True:
 			try:
@@ -28,31 +28,31 @@ def book_fitter(index):
 			except EOFError:
 				break
 		if not lines:
-			return (proceed_with_error(many_args, stdin_mode, index, READ_ERR))
+			return (print_proceed(many_args, std_input, index, READ_ERR))
 		shelves = lines[0].split()
 		lines.pop(0)
 		for line in lines:
 			new_book = line.split()[0]
 			books.append(new_book)
-	elif not stdin_mode:
+	elif not std_input:
 		try:
-			with open(sys.argv[index]) as file:
-				shelves = file.readline().split()		
-				line = file.readline()
+			with open(sys.argv[index]) as open_file:
+				shelves = open_file.readline().split()		
+				line = open_file.readline()
 				while line:
 					if line.split():
 						new_book = line.split()[0]
 					books.append(new_book)
-					line = file.readline()
-		except IOError as x:
-			return (proceed_with_error(many_args, False, index, READ_ERR))
+					line = open_file.readline()
+		except IOError:
+			return (print_proceed(many_args, False, index, READ_ERR))
 	try:
 		shelves.sort(reverse = True, key = int)
 		books.sort(reverse = True, key = int)
-	except ValueError as x:
-		return (proceed_with_error(many_args, stdin_mode, index, READ_ERR))
+	except ValueError:
+		return (print_proceed(many_args, std_input, index, READ_ERR))
 	if not shelves or not books:
-		return (proceed_with_error(many_args, stdin_mode, index, READ_ERR))
+		return (print_proceed(many_args, std_input, index, READ_ERR))
 	filled_shelves = []
 	while books:
 		if shelves:
@@ -60,16 +60,16 @@ def book_fitter(index):
 			shelves.pop(0)
 		i = 0
 		while i < len(books):
-			x = 0
-			while x < len(filled_shelves):
-				if int(books[i]) <= filled_shelves[x]:
-					filled_shelves[x] -= int(books[i])
+			j = 0
+			while j < len(filled_shelves):
+				if int(books[i]) <= filled_shelves[j]:
+					filled_shelves[j] -= int(books[i])
 					books[i] = 'X'
 					break
-				x += 1
+				j += 1
 			i += 1
 		if 'X' not in books:
-			return (proceed_with_error(many_args, stdin_mode, index, FIT_ERR))
+			return (print_proceed(many_args, std_input, index, FIT_ERR))
 		books[:] = (value for value in books if value != 'X')
 	if many_args:
 		print(f"{sys.argv[index]}:")

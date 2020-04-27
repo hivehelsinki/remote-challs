e3095824ebad409a494e8a48c4912b3e01c8707e
tdawson-42 <tdawson@student.hive.fi>
2020-04-27 16:04:13 +0300

    get rid of unnecessary function

--- chall07/tdawson.py
+++ chall07/tdawson.py
@@ -3,14 +3,6 @@
 import sys
 import numpy as np
 
-
-def print_outer(n):
-    for i in range(n):
-        path.append(array[0][i])
-    for i in range(1, n):
-        path.append(array[i][n-1])
-
-
 n = len(sys.argv) - 1
 
 if n < 1 or not all(arg.isdigit() and (len(arg) == n) for arg in sys.argv[1:]):
@@ -20,7 +12,8 @@ path = []
 array = np.array([[int(n) for n in row] for row in sys.argv[1:]])
 
 for i in range(n):
-    print_outer(n - i)
+    path.extend(list(p for p in array[0]))
+    path.extend(list(p for p in array[1:n, n-1-i]))
     array = np.rot90(array[1:, :n-1-i], 2)
 
 print(", ".join(str(n) for n in path))

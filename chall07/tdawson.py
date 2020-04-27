4ef977d556caa735aa28bc483355767f753f866a
tdawson-42 <tdawson@student.hive.fi>
2020-04-27 17:01:39 +0300

    fix incorrect range of valid input

--- chall07/tdawson.py
+++ chall07/tdawson.py
@@ -5,7 +5,7 @@ import numpy as np
 
 rows = len(sys.argv) - 1
 
-if rows not in range(1, 10) or not all(row.isdigit() and (len(row) == rows) for row in sys.argv[1:]):
+if rows not in range(1, 21) or not all(row.isdigit() and (len(row) == rows) for row in sys.argv[1:]):
     sys.exit(f'usage: {sys.argv[0]} <1-9 squared_rows...>')
 
 array = np.array([list(row) for row in sys.argv[1:]])

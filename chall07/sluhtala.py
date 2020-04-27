7d1311bc5b66909d0c8a97cee59eb695b0c9003c
Sasu Luhtala <sluhtala@me.com>
2020-04-27 23:04:24 +0300

    fixed usage name and added __name__ protection

--- chall07/sluhtala.py
+++ chall07/sluhtala.py
@@ -1,26 +1,10 @@
 #!/usr/bin/python3
 import sys
 
-
 def put_usage():
-    usage = "usage: ./xlogin.py <1-9 squared_rows...>"
+    usage = "usage: ./sluhtala.py <1-9 squared_rows...>"
     print(usage)
     exit()
-
-args = sys.argv
-if len(args) < 2:
-    put_usage
-args.pop(0)
-size = len(args)
-for i in range(size):
-    if len(args[i]) != size:
-        put_usage()
-nums = []
-for i in range(size):
-    for j in range(size):
-        if args[i][j].isnumeric() == 0:
-            put_usage()
-        nums.append(args[i][j])
 
 def print_sqr(nums, x, edge, new_nums):
     if edge == 1:
@@ -43,11 +27,26 @@ def print_sqr(nums, x, edge, new_nums):
     edge -= 1
     return int(edge)
 
-x = 0
-edge = size
-new_nums = []
-while edge > 0:
-    edge = print_sqr(nums, x, edge, new_nums)
-    x += size
-    x += 1
-print(*new_nums, sep = ", ") 
+if __name__ == "__main__":
+    args = sys.argv
+    if len(args) < 2:
+        put_usage
+    args.pop(0)
+    size = len(args)
+    for i in range(size):
+        if len(args[i]) != size:
+            put_usage()
+    nums = []
+    for i in range(size):
+        for j in range(size):
+         if args[i][j].isnumeric() == 0:
+              put_usage()
+         nums.append(args[i][j])
+    edge = size
+    new_nums = []
+    x = 0
+    while edge > 0:
+        edge = print_sqr(nums, x, edge, new_nums)
+        x += size
+        x += 1
+    print(*new_nums, sep = ", ") 

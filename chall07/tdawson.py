26cc62e769d8c789cb8e5b1f1e3874f01116878f
tdawson-42 <tdawson@student.hive.fi>
2020-04-27 16:48:34 +0300

    removed unnecessary typecasting

--- chall07/tdawson.py
+++ chall07/tdawson.py
@@ -8,7 +8,7 @@ rows = len(sys.argv) - 1
 if rows not in range(1, 10) or not all(row.isdigit() and (len(row) == rows) for row in sys.argv[1:]):
     sys.exit(f'usage: {sys.argv[0]} <1-9 squared_rows...>')
 
-array = np.array([[int(n) for n in row] for row in sys.argv[1:]])
+array = np.array([list(row) for row in sys.argv[1:]])
 
 path = []
 for _ in range(rows):
@@ -16,4 +16,4 @@ for _ in range(rows):
     path.extend(list(n for n in array[1:, -1]))
     array = np.rot90(array[1:, :-1], 2)
 
-print(", ".join(str(n) for n in path))
+print(", ".join(path))

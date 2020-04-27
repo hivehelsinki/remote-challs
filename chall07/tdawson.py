682f8afac2e2449131a1f45da9cd809ba08092ba
tdawson-42 <tdawson@student.hive.fi>
2020-04-27 16:42:06 +0300

    use -1 for last index, changed variable names

--- chall07/tdawson.py
+++ chall07/tdawson.py
@@ -3,17 +3,17 @@
 import sys
 import numpy as np
 
-n = len(sys.argv) - 1
+rows = len(sys.argv) - 1
 
-if n < 1 or not all(arg.isdigit() and (len(arg) == n) for arg in sys.argv[1:]):
+if rows not in range(1, 10) or not all(row.isdigit() and (len(row) == rows) for row in sys.argv[1:]):
     sys.exit(f'usage: {sys.argv[0]} <1-9 squared_rows...>')
 
-path = []
 array = np.array([[int(n) for n in row] for row in sys.argv[1:]])
 
-for i in range(n):
-    path.extend(list(p for p in array[0]))
-    path.extend(list(p for p in array[1:n, n-1-i]))
-    array = np.rot90(array[1:, :n-1-i], 2)
+path = []
+for _ in range(rows):
+    path.extend(list(n for n in array[0]))
+    path.extend(list(n for n in array[1:, -1]))
+    array = np.rot90(array[1:, :-1], 2)
 
 print(", ".join(str(n) for n in path))

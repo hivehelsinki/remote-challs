dd5d63c7bd840ad4e69a6d79c6ae40b93a12090f
Sasu Luhtala <sluhtala@me.com>
2020-05-04 19:28:08 +0300

    added ie_ei_c_challenge

---
+++ chall08/sluhtala.c
@@ -0,0 +1,27 @@
+#include <string.h>
+
+int ft_ie_except_after_c(char *str)
+{
+	int i;
+
+	i = 0;
+	if (str == NULL)
+		return (-1);
+	if (strlen(str) == 0)
+		return (1);
+	if (str[0] == 'e' && str[1] == 'i')
+		return (0);
+	while (str[i])
+	{
+		if (str[i] != 'c' && str[i + 1] == 'e' && str[i + 2] == 'i')
+		{
+			return (0);				
+		}
+		if (str[i] == 'c' && str[i + 1] == 'i' && str[i + 2] == 'e')
+		{
+			return (0);
+		}
+		i++;
+	} 
+	return (1);
+}

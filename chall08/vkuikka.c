605a7167c5d8768b1cce3fc8beff7f51d446b9b2
vkuikka <villekuikka1@gmail.com>
2020-05-04 16:58:07 +0300

    chall08/vkuikka.c made code clearer and took out a variable that was not needed anymore

--- chall08/vkuikka.c
+++ chall08/vkuikka.c
@@ -1,18 +1,17 @@
 int	ft_ie_except_after_c(char *str)
 {
-	int		follows_rule;
-
-	follows_rule = 1;
 	if (*str == 'e' && *(str + 1) == 'i')
 		return (0);
 	while (*(str++))
 	{
-		if (*str == 'i' && *(str + 1) == 'e')
-			follows_rule = (*(str - 1) != 'c');
-		else if (*str == 'e' && *(str + 1) == 'i')
-			follows_rule = (*(str - 1) == 'c');
-		if (!follows_rule)
+		if (*str == 'i' &&
+			*(str + 1) == 'e' &&
+			*(str - 1) == 'c')
+			return (0);
+		else if (*str == 'e' &&
+			*(str + 1) == 'i' &&
+			*(str - 1) != 'c')
 			return (0);
 	}
-	return (follows_rule);
+	return (1);
 }

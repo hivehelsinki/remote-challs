a10721536271514472a8e257e5df732d65297f9e
hiiri <alcohen@student.hive.fi>
2020-05-04 16:31:53 +0300

    Add my solution to chall08

---
+++ chall08/alcohen.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+
+/*
+**  Return 1 if "ei" after "c", 0 otherwise
+*/
+static int check_ei(char *str, size_t i)
+{
+    if (str[i + 1] && str[i + 1] == 'i')
+    {
+        if (str[i - 1] && str[i - 1] == 'c')
+            return (1);
+        else
+            return (0);
+    }
+    return (1);
+}
+
+/*
+**  Return 0 if "ie" after "c", 1 otherwise
+*/
+static int check_ie(char *str, size_t i)
+{
+    if (str[i + 1] && str[i + 1] == 'e')
+        {
+            if (str[i - 1] && str[i - 1] == 'c')
+                return (0);
+        }
+    return (1);
+}
+
+
+int ft_ie_except_after_c(char* str)
+{
+    size_t  i;
+
+    i = 0;
+    while (str[i])
+    {
+        if (str[i] == 'e')
+        {
+            if (check_ei(str, i) == 0)
+                return (0);
+        }
+        else if (str[i] == 'i')
+            if (check_ie(str, i) == 0)
+                return (0);
+        i++;
+    }
+    return (1);
+}

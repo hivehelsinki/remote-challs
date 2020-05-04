dbd5aea31f976882e998ab97badc980594bb3e71
RandommUser <RandommUser@gmail.com>
2020-05-04 16:13:07 +0300

    Well yes, but actually no
    English grammar rules don't make sense, but still better than the pronounciations.
    
    So, just a simple string looper. Get the string len, have a counter to keep track of positions, sub-function that checks if previous letter was 'c' and next is the third part  of the combo. Inverse the reusult for the "must not" rule.
    
    Default to ret 1 and only change it if the the rule is broken. Could have made it into all one function but somewhat tried to follow the Norm.

---
+++ chall08/phakakos.c
@@ -0,0 +1,36 @@
+long	ft_strlen(char *str)
+{
+	long	i;
+	
+	i = 0;
+	while (str[i])
+		i++;
+	return (i)
+}
+
+int		following(char *str, char c)
+{
+	if (str[0] == 'c' && str[2] == c)
+		return (1);
+	return (0);
+}
+
+int		ft_ie_except_after_c(char *str)
+{
+	int		rtn;
+	long	len;
+	long	i;
+	
+	i = 0;
+	len = ft_strlen(str) - 1;
+	rtn = 1;
+	while (i < len && rtn == 1)
+	{
+		if (str[i] == 'e' && i > 0)
+			rtn = following(&str[i - 1], 'i');
+		else if (str[i] == 'i' && i > 0)
+			rtn = !following(&str[i - 1], 'e')
+		i++;
+	}
+	return (rtn)
+}

ee98928bc4ca6a342f36722a9a02aafb2593c77b
rasmusjaa <rasmus@lense.fi>
2020-05-04 16:50:19 +0300

    changed to for loop and put if statements together to make it more compact

--- chall08/rjaakonm.c
+++ chall08/rjaakonm.c
@@ -1,16 +1,13 @@
 int ft_ie_except_after_c(char *str)
 {
-	int i = 0;
-
-	while (str[i])
+	for (int i = 0; str[i]; i++)
 	{
-		if (str[i] == 'i' && str[i + 1] == 'e')
-			if (i > 0 && str[i - 1] == 'c')
+		if (str[i] == 'i' && str[i + 1] == 'e' 
+			&& i != 0 && str[i - 1] == 'c')
 				return (0);
-		if (str[i] == 'e' && str[i + 1] == 'i')
-			if (i == 0 || str[i - 1] != 'c')
+		if (str[i] == 'e' && str[i + 1] == 'i' 
+			&& (i == 0 || str[i - 1] != 'c'))
 				return (0);
-		i++;
 	}
 	return (1);
 }

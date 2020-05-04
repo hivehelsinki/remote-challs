5c00fd488576f460c789da0d441b74a7830132aa
vkuikka <villekuikka1@gmail.com>
2020-05-04 16:45:06 +0300

    made the function stop immediately if the input does not follow the rule and added a check before starting the loop because it skips the first character

--- chall08/vkuikka.c
+++ chall08/vkuikka.c
@@ -3,12 +3,16 @@ int	ft_ie_except_after_c(char *str)
 	int		follows_rule;
 
 	follows_rule = 1;
+	if (*str == 'e' && *(str + 1) == 'i')
+		return (0);
 	while (*(str++))
 	{
 		if (*str == 'i' && *(str + 1) == 'e')
 			follows_rule = (*(str - 1) != 'c');
 		else if (*str == 'e' && *(str + 1) == 'i')
 			follows_rule = (*(str - 1) == 'c');
+		if (!follows_rule)
+			return (0);
 	}
 	return (follows_rule);
 }

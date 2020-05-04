9c974264d062d9c553a39142bf9b6a7b602aaca6
T7q <tkuumola@gmail.com>
2020-05-04 20:59:33 +0300

    update2

--- chall08/tkuumola.c
+++ chall08/tkuumola.c
@@ -1,12 +1,33 @@
 #include <string.h>
 
+int match(char *str, char *needle)
+{
+    int flag = 0;
+
+    while( (str=strstr(str, needle)) != NULL)
+    {
+        flag = 1;
+        str++;
+    }
+    return (flag);
+}
+
 int ft_ie_except_after_c(char *str)
 {    
     if (str == NULL || strlen(str) == 0)
         return(0);
-    if (strstr(str, "ei") != NULL)
-        return (strstr(str, "cei") != NULL ? 1 : 0);
-    if (strstr(str, "ie") != NULL)
-        return (strstr(str, "cie") != NULL ? 0 : 1);
-    return(1);
+    
+    if (match(str, "ei") == 0 && match(str, "ie") == 0)
+        return (1);
+
+    int cei_word = match(str, "cei");
+    int ei_word = match(str, "ei");
+    int cie_word = match(str, "cie");
+    int ie_word = match(str, "ie");
+
+    if (cei_word == 1 && cie_word == 0) 
+        return (1);
+    if (ie_word == 1 && cie_word == 0 && ei_word == 0 && strlen(str) > 2)
+        return (1);
+    return (0);
 }

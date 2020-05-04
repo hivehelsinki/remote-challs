c0c6bceb59b7221dba57df5613f58e3ca3b2f1df
r1 <r1@87-92-19-44.bb.dnainternet.fi>
2020-05-04 16:46:51 +0300

    08.6

--- chall08/rpehkone.c
+++ chall08/rpehkone.c
@@ -2,12 +2,12 @@ int	ft_ie_except_after_c(char *str)
 {
 	for (int i = 0; str[i]; i++)
 	{
-		if (str[i] == 'e' && str[i + 1] && str[i + 1] == 'i')
+		if (str[i] == 'e' && str[i + 1] == 'i')
 		{
 			if (!i || str[i - 1] != 'c')
 				return (0);
 		}
-		else if (str[i] == 'i' && str[i + 1] && str[i + 1] == 'e')
+		else if (str[i] == 'i' && str[i + 1] == 'e')
 			if (i && str[i - 1] == 'c')
 				return (0);
 	}

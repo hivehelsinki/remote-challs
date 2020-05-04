14d02c8bc23822f81823ecd8c6c597106a93afb8
rasmusjaa <rasmus@lense.fi>
2020-05-04 20:07:39 +0300

    changed cie check to simple

--- chall08/rjaakonm.c
+++ chall08/rjaakonm.c
@@ -2,11 +2,9 @@ int ft_ie_except_after_c(char *str)
 {
 	for (int i = 0; str[i]; i++)
 	{
-		if (str[i] == 'i' && str[i + 1] == 'e' 
-			&& i != 0 && str[i - 1] == 'c')
+		if (str[i] == 'c' && str[i + 1] == 'i' && str[i + 2] == 'e')
 				return (0);
-		if (str[i] == 'e' && str[i + 1] == 'i' 
-			&& (i == 0 || str[i - 1] != 'c'))
+		if (str[i] == 'e' && str[i + 1] == 'i' && (i == 0 || str[i - 1] != 'c'))
 				return (0);
 	}
 	return (1);

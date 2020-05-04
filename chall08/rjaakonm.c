7cf2a0e2bd0440f0c30f43d6a079cd04fac971c2
rasmusjaa <rasmus@lense.fi>
2020-05-04 20:13:45 +0300

    indentation

--- chall08/rjaakonm.c
+++ chall08/rjaakonm.c
@@ -3,9 +3,9 @@ int ft_ie_except_after_c(char *str)
 	for (int i = 0; str[i]; i++)
 	{
 		if (str[i] == 'c' && str[i + 1] == 'i' && str[i + 2] == 'e')
-				return (0);
+			return (0);
 		if (str[i] == 'e' && str[i + 1] == 'i' && (i == 0 || str[i - 1] != 'c'))
-				return (0);
+			return (0);
 	}
 	return (1);
 }
